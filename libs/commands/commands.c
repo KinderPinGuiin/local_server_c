f0b82e14be84c0bcd4c5a652107ddc5fc61d35c6
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2021-12-30 17:19:26 +0100

    Mise à jour de l'aide

--- libs/commands/commands.c
+++ libs/commands/commands.c
@@ -81,16 +81,20 @@ void print_commands() {
     "    - \033[0;36mls ...\033[0m : Toutes les variantes de ls.\n"
     "    - \033[0;36mps ...\033[0m : Toutes les variantes de ps.\n"
     "    - \033[0;36mpwd ... \033[0m : Toutes les variantes de pwd.\n"
+    "    - \033[0;36mrm ... \033[0m : Toutes les variantes de rm.\n"
+    "    - \033[0;36mmkdir ... \033[0m : Toutes les variantes de mkdir.\n"
+    "    - \033[0;36mtouch ... \033[0m : Toutes les variantes de touch.\n"
     "    - \033[0;36mexit\033[0m : Permet de se déconnecter du "
       "serveur.\n"
     "Liste des commandes personnalisées disponibles :\n"
     "    - \033[0;36minfo <PID>\033[0m : Affiche sur la sortie standard les "
       "informations concernant le processus de numéro PID.\n"
-    "    - \033[0;36mccp <src> <dest> -[v|a|b|e]\033[0m : Copie le fichier src "
+    "    - \033[0;36mccp -f <src> -d <dest> -[v|a|b|e]\033[0m : Copie src "
       "dans le fichier dest. -v permet de vérifier si le fichier existe déjà, "
       "-a permet de copier en mode ajout, -b et -e permettent respectivement "
       "de définir un offset de début et de fin.\n"
     "    - \033[0;36mlsl\033[0m : Commande raccourcie de ls -ali.\n"
+    "    - \033[0;36muinfo\033[0m : Vos informations utilisateurs.\n"
   );
 }
 
